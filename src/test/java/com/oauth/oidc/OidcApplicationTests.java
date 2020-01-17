9cf6f18fc0391954c8324c085a88f705d33fd1f7
DEL BARRIO PIZARRO Bladimir <b.delbarriopizarro@soprasteria.com>
2020-01-17 12:13:17 +0100

    first commit

---
+++ src/test/java/com/oauth/oidc/OidcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauth.oidc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OidcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
